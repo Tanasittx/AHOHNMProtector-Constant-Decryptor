dadc34e9aad14fba73a6f78e654ff9b33f3ffa12
Tanasittx <god_5631@hotmail.com>
2020-05-07 20:38:29 +0700

    Add files via upload

---
+++ ConfuserExConstantDecryptor/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mihai
+ * Date: 5/7/2015
+ * Time: 2:51 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ConfuserExConstantDecryptor
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(393, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 26);
+            this.button3.TabIndex = 35;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 26);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 26);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "Browse for assembly";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(112, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(293, 22);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Current status";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(46, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 14);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Name of assembly:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(46, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(419, 20);
+            this.textBox1.TabIndex = 30;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBox1DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextBox1DragEnter);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 152);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "АнонимProtector Constant Decryptor | Tanasittx";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+	}
+}
---
+++ ConfuserExConstantDecryptor/MainForm.cs
@@ -0,0 +1,327 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mihai
+ * Date: 5/7/2015
+ * Time: 2:51 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Reflection;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+using dnlib.DotNet.Emit;
+
+namespace ConfuserExConstantDecryptor
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void TextBox1DragDrop(object sender, DragEventArgs e)
+		{
+		try
+        { 
+        Array a = (Array) e.Data.GetData(DataFormats.FileDrop);
+        if(a != null)
+        {
+        string s = a.GetValue(0).ToString();
+        int lastoffsetpoint = s.LastIndexOf(".");
+        if (lastoffsetpoint != -1)
+        {
+        string Extension = s.Substring(lastoffsetpoint);
+        Extension=Extension.ToLower();
+        if (Extension == ".exe"||Extension == ".dll")
+        {
+        this.Activate();
+        textBox1.Text = s;
+        int lastslash = s.LastIndexOf("\\");
+        if (lastslash!=-1) DirectoryName = s.Remove(lastslash,s.Length-lastslash);
+        if (DirectoryName.Length==2) DirectoryName=DirectoryName+"\\";
+        }
+        }
+        }
+        }
+        catch
+        {
+
+        }
+		}
+		
+		void TextBox1DragEnter(object sender, DragEventArgs e)
+		{
+		if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        e.Effect = DragDropEffects.Copy;
+    	else
+    	e.Effect = DragDropEffects.None;
+		}
+		
+		public string DirectoryName = "";
+		void Button1Click(object sender, EventArgs e)
+		{
+		label2.Text="";
+		OpenFileDialog fdlg = new OpenFileDialog();
+		fdlg.Title = "Browse for target assembly";
+		fdlg.InitialDirectory = @"c:\";
+		if (DirectoryName!="") fdlg.InitialDirectory = DirectoryName;
+		fdlg.Filter = "All files (*.exe,*.dll)|*.exe;*.dll";
+		fdlg.FilterIndex = 2;
+		fdlg.RestoreDirectory = true;
+		if(fdlg.ShowDialog() == DialogResult.OK)
+		{
+		string FileName = fdlg.FileName;
+		textBox1.Text = FileName;
+		int lastslash = FileName.LastIndexOf("\\");
+		if (lastslash!=-1) DirectoryName = FileName.Remove(lastslash,FileName.Length-lastslash);
+        if (DirectoryName.Length==2) DirectoryName=DirectoryName+"\\";
+		}
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+		Application.Exit();
+		}
+		
+		List<MethodDef> methods = new List<MethodDef>();
+		
+		public void AddMethods(TypeDef type)
+		{
+		if (type.HasMethods)
+		{
+		        foreach (MethodDef method in type.Methods)
+                {
+                if (method.HasBody)
+                methods.Add(method);
+                }
+		}
+		    if (type.HasNestedTypes)
+            {
+		    	foreach (TypeDef nestedtype in type.NestedTypes)
+                {
+                AddMethods(nestedtype);
+                }
+		    }
+		}
+		
+public MethodInfo GetStringMethod(int MethodToken)
+{
+    MethodInfo info = (MethodInfo) assembly.ManifestModule.ResolveMethod(MethodToken);
+    if (info.IsGenericMethodDefinition || info.IsGenericMethod)
+    {
+        return info.MakeGenericMethod(new Type[] { typeof(string) });
+    }
+    return info;
+}
+	
+public MethodInfo GetIntMethod(int MethodToken)
+{
+    MethodInfo info = (MethodInfo) assembly.ManifestModule.ResolveMethod(MethodToken);
+    if (info.IsGenericMethodDefinition || info.IsGenericMethod)
+    {
+        return info.MakeGenericMethod(new Type[] { typeof(int) });
+    }
+    return info;
+}
+
+public MethodInfo GetFloatMethod(int MethodToken)
+{
+    MethodInfo info = (MethodInfo) assembly.ManifestModule.ResolveMethod(MethodToken);
+    if (info.IsGenericMethodDefinition || info.IsGenericMethod)
+    {
+        return info.MakeGenericMethod(new Type[] { typeof(float) });
+    }
+    return info;
+}
+		Assembly assembly;
+		void Button2Click(object sender, EventArgs e)
+		{
+	if (File.Exists(textBox1.Text))
+	{
+AssemblyName an = AssemblyName.GetAssemblyName(textBox1.Text);
+assembly = Assembly.Load(an);
+if (assembly==null)
+{
+MessageBox.Show("Failed to load the assembly! Aborting!");
+return;
+}
+string directoryname = Path.GetDirectoryName(textBox1.Text);
+if (!directoryname.EndsWith("\\")) directoryname=directoryname+"\\";
+string newfilename = directoryname+Path.GetFileNameWithoutExtension(textBox1.Text)+"_constantsdec"+Path.GetExtension(textBox1.Text);
+
+	// Open or create an assembly
+	 AssemblyDef asm = AssemblyDef.Load(textBox1.Text);
+	 ModuleDef mod = asm.ManifestModule;
+	if (mod.IsILOnly)
+	{
+    ModuleWriterOptions writerOptions = new ModuleWriterOptions(mod);
+    writerOptions.MetaDataOptions.Flags |= MetaDataFlags.PreserveUSOffsets | MetaDataFlags.PreserveBlobOffsets | MetaDataFlags.PreserveExtraSignatureData | MetaDataFlags.PreserveAllMethodRids | MetaDataFlags.PreserveFieldRids | MetaDataFlags.PreserveParamRids | MetaDataFlags.PreserveTypeRefRids | MetaDataFlags.PreserveTypeDefRids | MetaDataFlags.PreservePropertyRids | MetaDataFlags.PreserveTypeSpecRids | MetaDataFlags.PreserveStandAloneSigRids | MetaDataFlags.PreserveEventRids | MetaDataFlags.KeepOldMaxStack;
+	methods = new List<MethodDef>();
+	if (mod.HasTypes)
+	{
+    foreach (TypeDef typedef in mod.Types)
+    {
+	AddMethods(typedef);
+    }
+	}
+	
+	int replacecount = 0;
+	for (int i = 0; i < methods.Count; i++) // Loop with for.
+	{  // search on all methods for strings:
+	for (int j = 0x0; j < methods[i].Body.Instructions.Count; j++)
+    {
+	if (methods[i].Body.Instructions[j].OpCode == OpCodes.Call && 
+		  methods[i].Body.Instructions[j].Operand.ToString().Contains("String>")
+		  &&(j-01)>=0&&methods[i].Body.Instructions[j - 0x1].Operand.ToString() == "Encrypted By Nightbaron"
+        && (j-02)>=0&&methods[i].Body.Instructions[j - 0x2].IsLdcI4()  )
+
+
+        {
+                                MethodInfo method = null;
+                                object returnedstring = null;
+                            
+                                MessageBox.Show("Found String!");
+                                try
+                                {
+                                    if (methods[i].Body.Instructions[j].Operand.GetType().ToString().Contains("MethodDef"))
+                                    {
+                                        method = GetStringMethod((int) ((MethodDef) methods[i].Body.Instructions[j].Operand).MDToken.Raw);
+                                    }
+                                    else                              
+                                    {
+                                        method = GetStringMethod((int) ((MethodSpec) methods[i].Body.Instructions[j].Operand).Method.MDToken.Raw);
+                                    }
+                                    string stringtext1 = methods[i].Body.Instructions[j - 0x1].Operand.ToString();
+                                    int intergerevalue1 = methods[i].Body.Instructions[j - 0x2].GetLdcI4Value();
+                                    uint intergerevalue2 = (uint)methods[i].Body.Instructions[j - 0x3].GetLdcI4Value();
+                                    int intergerevalue3 = methods[i].Body.Instructions[j - 0x4].GetLdcI4Value();
+                                    uint intergerevalue4 = (uint)methods[i].Body.Instructions[j - 0x5].GetLdcI4Value();
+                                    returnedstring = method.Invoke(null, new object[] { intergerevalue4,intergerevalue3, intergerevalue2, intergerevalue1,stringtext1 });
+                                    MessageBox.Show(returnedstring.ToString());
+                                }
+                                catch (Exception exc)
+                                {
+                                    int whatthehell = 1;
+                                }
+                                if (returnedstring != null)
+                                {
+                                    methods[i].Body.Instructions[j - 0x1].OpCode = OpCodes.Nop;
+                                    methods[i].Body.Instructions[j - 0x2].OpCode = OpCodes.Nop;
+                                    methods[i].Body.Instructions[j - 0x3].OpCode = OpCodes.Nop;
+                                    methods[i].Body.Instructions[j - 0x4].OpCode = OpCodes.Nop;
+                                    methods[i].Body.Instructions[j - 0x5].OpCode = OpCodes.Nop;
+                                    methods[i].Body.Instructions[j].OpCode = OpCodes.Ldstr;
+                                    methods[i].Body.Instructions[j].Operand = (string) returnedstring;
+                                    replacecount++;
+                                }
+
+                            } // end of string decryption
+
+		
+		
+		//if (methods[i].Body.Instructions[j].OpCode == OpCodes.Call && 
+		//  methods[i].Body.Instructions[j].Operand.ToString().Contains("Int32>")
+		//  &&(j-01)>=0&&methods[i].Body.Instructions[j - 0x1].IsLdcI4()
+  //        && (j - 02) >= 0 && methods[i].Body.Instructions[j - 0x2].IsLdcI4())
+  //      {
+		//	MethodInfo method = null;
+  //          object returnedint = null;
+  //          try
+  //          {
+
+  //              if (methods[i].Body.Instructions[j].Operand.GetType().ToString().Contains("MethodDef"))
+  //              {
+  //                  method = GetIntMethod((int) ((MethodDef) methods[i].Body.Instructions[j].Operand).MDToken.Raw);
+  //              }
+  //              else
+  //              {
+  //                  method = GetIntMethod((int) ((MethodSpec) methods[i].Body.Instructions[j].Operand).Method.MDToken.Raw);
+  //              }
+  //                                  uint intergerevalue1 = (uint)methods[i].Body.Instructions[j - 0x1].GetLdcI4Value();
+  //                                  uint intergerevalue2 = (uint)methods[i].Body.Instructions[j - 0x2].GetLdcI4Value();
+  //                                  returnedint = method.Invoke(null, new object[] { intergerevalue2, intergerevalue1 });
+  //                              }
+  //          catch
+  //          {
+  //          }
+            
+  //          if (returnedint != null)
+  //          {
+  //                                  methods[i].Body.Instructions[j - 0x1].OpCode = OpCodes.Nop;
+  //                                  methods[i].Body.Instructions[j - 0x2].OpCode = OpCodes.Nop;
+  //                                  methods[i].Body.Instructions[j].OpCode = OpCodes.Ldc_I4;
+  //              methods[i].Body.Instructions[j].Operand = (int) returnedint;
+  //              replacecount++;
+  //          }
+
+
+	 // } // end of int decryption 
+		
+		
+		
+		//	if (methods[i].Body.Instructions[j].OpCode == OpCodes.Call && 
+		//  methods[i].Body.Instructions[j].Operand.ToString().Contains("Single>")
+		//  &&(j-01)>=0&&methods[i].Body.Instructions[j - 0x1].IsLdcI4()
+  //        && (j - 02) >= 0 && methods[i].Body.Instructions[j - 0x2].IsLdcI4())
+  //      {
+		//	MethodInfo method = null;
+  //          object returnedfloat = null;
+  //          try
+  //          {
+
+  //              if (methods[i].Body.Instructions[j].Operand.GetType().ToString().Contains("MethodDef"))
+  //              {
+  //                  method = GetFloatMethod((int) ((MethodDef) methods[i].Body.Instructions[j].Operand).MDToken.Raw);
+  //              }
+  //              else
+  //              {
+  //                  method = GetFloatMethod((int) ((MethodSpec) methods[i].Body.Instructions[j].Operand).Method.MDToken.Raw);
+  //              }
+  //                                  uint intergerevalue1 = (uint)methods[i].Body.Instructions[j - 0x1].GetLdcI4Value();
+  //                                  uint intergerevalue2 = (uint)methods[i].Body.Instructions[j - 0x2].GetLdcI4Value();
+  //                                  returnedfloat = method.Invoke(null, new object[] { intergerevalue2, intergerevalue1 });
+  //                              }
+  //          catch
+  //          {
+  //          }
+            
+  //          if (returnedfloat != null)
+  //          {
+  //                                  methods[i].Body.Instructions[j - 0x1].OpCode = OpCodes.Nop;
+  //                                  methods[i].Body.Instructions[j - 0x2].OpCode = OpCodes.Nop;
+  //                                  methods[i].Body.Instructions[j].OpCode = OpCodes.Ldc_R4;
+  //              methods[i].Body.Instructions[j].Operand = (float) returnedfloat;
+  //              replacecount++;
+  //          }
+
+
+	 // } // end of float decryption */
+		
+	}
+	}
+
+                    writerOptions.Logger = DummyLogger.NoThrowInstance;
+                     mod.Write(newfilename, writerOptions);
+                    label2.Text = replacecount.ToString()+" constants decrypted!";
+    
+	}
+	}
+		}
+	}
+}
